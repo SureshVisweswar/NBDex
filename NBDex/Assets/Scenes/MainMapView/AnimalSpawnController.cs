6e12fb47dcdd68137afbe1fa0b1c40429b760fb1
PrimaStudios <primastudioscanada@gmail.com>
2020-12-03 00:21:17 -0400

    Saving progress, got map tweaked, added gui, added player model, started animal spawn logic

---
+++ NBDex/Assets/Scenes/MainMapView/AnimalSpawnController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mapbox.Unity.Location;
+using Mapbox.Utils;
+
+public class AnimalSpawnController : MonoBehaviour
+{
+    [SerializeField] private LocationProviderFactory locationProvider;
+    
+    private float nextSpawnActionTime = 0.0f;
+    public float period = 10.0f;
+
+    //Spawn Rates
+    private double peregrineFalcon = 0.1;
+
+    //Spawn Locations
+    private double[] spawnRange = {45.47168826, -66.42745972, 45.23331631, -65.72502136};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //double locX = locationProvider.CurrentLocation.LatitudeLongitude.x;
+        //double locY = locationProvider.CurrentLocation.LatitudeLongitude.y;
+    }
+}
---
+++ NBDex/Assets/Scenes/MainMapView/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Button profileButton;
+    [SerializeField] private Button NBDexButton;
+
+    private void Awake() {
+        Assert.IsNotNull(profileButton);
+        Assert.IsNotNull(NBDexButton);
+    }
+
+    public void openProfileMenu() {
+        Debug.Log("In Prof Func");
+    }
+
+    public void openNBDex() {
+        Debug.Log("In NB Func");
+    }
+}
